7acb88a9d7dad06dc59e738d9c0d3b74a4c77c55
GustavoCapalbo <gustavocsantos16032003@gmail.com>
2024-01-19 16:40:18 -0300

    h1 degrade a pedido do cliente
    fiz um jogo de cores para dar ênfase na marca do cliente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Este script é opcional, mas pode ser útil para ocultar o botão em dispositivos pequenos
 window.addEventListener('resize', function() {
-    if (window.innerWidth <= 700) {
+    if (window.innerWidth <= 300) {
         document.getElementById('whatsapp-button').style.display = 'none';
     } else {
         document.getElementById('whatsapp-button').style.display = 'block';
